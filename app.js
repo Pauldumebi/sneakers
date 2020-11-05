c9cda652e5b0e566943a355da22425c45495ecab
Chukwurah Paul <paulchukwurah7@gmail.com>
2020-11-05 10:37:44 +0100

    fixed links

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-let blueColor = document.querySelector('.blue').addEventListener('click', function blueColor () {
+let pinkButton = document.querySelector('.submitBtn')
+    blueColor = document.querySelector('.blue').addEventListener('click', function blueColor () {
+      // To change the background colour to blue when clicked
       document.querySelector('body').style.backgroundColor = '#0392BF'
-})
+   })
     pinkColor = document.querySelector('.pink').addEventListener('click', 
-   function pinkColor () {
-   document.querySelector('body').style.backgroundColor = '#ED48BA'
-})
+    function pinkColor () {
+      // To change the background colour to pink when clicked
+      document.querySelector('body').style.backgroundColor = '#ED48BA';
+   })
     yellowColor = document.querySelector('.yellow').addEventListener('click', function yellowColor() {
-       document.querySelector('body').style.backgroundColor = '#FBEC06'
-    })
+      // To change the background colour to yellow when clicked
+      document.querySelector('body').style.backgroundColor = '#FBEC06'
+   })
     greyColor = document.querySelector('.grey').addEventListener('click', function greyColor() {
-       document.querySelector('body').style.backgroundColor = '#B3D4E0'
-    })
-    colorSection = document.getElementById('color-section');
-    allColours = colorSection.getElementsByClassName('color')
-    for (let i = 0; i < allColours.length; i++) {
-      allColours[i].addEventListener("click", function() {
-      var current = document.getElementsByClassName("active");
-      current[0].className = current[0].className.replace(" active", "");
-      this.className += " active";
-      });
+      // To change the background colour to grey when clicked
+      document.querySelector('body').style.backgroundColor = '#B3D4E0'
+   })
 
-    }
+// Adding the active class to selected buttons
+colorSection = document.getElementById('color-section');
+allColours = colorSection.getElementsByClassName('color')
+for (let i = 0; i < allColours.length; i++) {
+   allColours[i].addEventListener("click", function() {
+   var current = document.getElementsByClassName("active");
+   current[0].className = current[0].className.replace(" active", "");
+   this.className += " active";   
+   })
+}
+// allColours = document.getElementsByClassName('color')
+// for (let i = 0; i < allColours.length; i++) {
+//    if (allColours[3] = 'clicked') {
+//       pinkButton.classList.add('pink-active')
+//    }  else {
+//       pinkButton.classList.remove('pink-active')
+//    }
+// };
+
 
     
